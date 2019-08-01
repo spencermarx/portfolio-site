5644a05dc26bdbcc718011111cae7f55acdeb8ae
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-01 07:51:15 -0400

    Include bootstrap minified CSS in Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 class Layout extends Component{
     render(){
         return(
