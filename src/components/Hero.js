66dc6fe41cbc9d258dab2558e8b0f133979d8cfb
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-01 09:32:26 -0400

    Add Hero section to Home page

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <section id="Hero">
+                    <div className="my-5 text-center">
+                        <h1>Full Stack Developer with<br />expertise in UX Design and GIS</h1>
+                    </div>
+                </section>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Hero;
