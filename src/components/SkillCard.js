2d245d824a1be46773a1f8f14b80fd708373db18
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-02 10:27:06 -0400

    Add SkillCard component and populate using GraphQL and Contentful

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import './skillcard.scss';
+
 
 class SkillCard extends Component {
     render() {
+        const title = this.props.title
+        const image = this.props.imageURL
+        const skillArray = this.props.skills
         return (
             <React.Fragment>
-                <h1>Hi</h1>
+                <Card className="SkillCard">
+                    <img className="SkillCard-Image" variant="top" src={image} alt="Icon" />
+                    <Card.Body className="text-center">
+                        <Card.Title className="SkillCard-Title">{title}</Card.Title>
+                        <ul className="SkillCard-Skills">
+                            {skillArray.map(skill =>
+                                <p className="mb-2 text-muted">{skill}</p>
+                            )}
+                        </ul>
+                    </Card.Body>
+                </Card>
             </React.Fragment>
         )
     }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import SkillCard from './SkillCard';
 import { Row, Col } from 'react-bootstrap';
 
-class Skills extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <section id="Skills">
-                    <Row>
-                        <Col xs={12} md={6} xl={4}>
-                            <SkillCard/>
+const Skills = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        allContentfulSkillSet(sort: { fields: pageOrder, order: ASC }) {
+            edges {
+            node {
+                title
+                image {
+                file {
+                    url
+                }
+                }
+                skills
+                pageOrder
+            }
+            }
+        }
+        }
+    `);
+
+    const skillCards = data.allContentfulSkillSet.edges;
+
+    return (
+        <React.Fragment>
+            <section id="Skills">
+                <Row>
+                    {skillCards.map(edge =>
+                        <Col xs={12} sm={6} lg={3} className="d-flex justify-content-center">
+                            <SkillCard
+                            title = {edge.node.title}
+                            imageURL = {edge.node.image.file.url }
+                            skills={edge.node.skills}/>
                         </Col>
-                        <Col xs={12} md={6} xl={4}>
-                            <SkillCard/>
-                        </Col>
-                        <Col xs={12} md={6} xl={4}>
-                            <SkillCard/>
-                        </Col>
-                    </Row>
-                </section>
-            </React.Fragment>
-        )
-    }
+                    )}
+                </Row>
+            </section>
+        </React.Fragment>
+    )
 }
+
 
 export default Skills;
