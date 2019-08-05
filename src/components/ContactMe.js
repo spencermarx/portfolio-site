be8f176d221d45ceafffc3a4e97cbe361b1cf419
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-05 08:44:07 -0400

    Add Contact Me section to Home page

---
+++ src/components/ContactMe.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import { Row, Button } from 'react-bootstrap';
+import './contactme.scss'
+
+class ContactMe extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <section id="ContactMe" className="my-5">
+                    <h1 className="text-center">Got a question?</h1>
+                    <p className="text-center">Don’t be shy! I’d love to help in any way that I can!</p>
+                    <Row>
+                        <Link to="/contact" className="mx-auto"><Button variant="dark" className="ContactMe-Button">Say Hi!</Button></Link>
+                    </Row>
+                </section>
+            </React.Fragment>
+        )
+    }
+}
+
+
+export default ContactMe;
