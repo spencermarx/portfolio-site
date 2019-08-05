43398e1cc3da0ef588cd862353ab9d7c9ee29805
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-05 17:58:59 -0400

    Add Contact page and ContactForm component

---
+++ src/pages/contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import ContactForm from '../components/ContactForm';
+import { Container } from 'react-bootstrap';
+
+class ContactPage extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Layout>
+                    <Container>
+                        <h1 className="text-center mt-4">Contact Me</h1>
+                        <p className="text-center">Have a question or want to work together?</p>
+                        <ContactForm />
+                    </Container>
+                </Layout>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ContactPage;
