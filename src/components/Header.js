60ba9778b93b6c66ec4b0993172e3681de5206ba
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-07 07:42:39 -0400

    Fix header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,19 +9,17 @@ class Header extends Component {
     render() {
         return (
             <React.Fragment>
-                <SiteMetaData/>
+                <SiteMetaData />
                 <Navbar bg="transparent" expand="lg">
                     <Container>
-                        <Link to="/">
-                            <Navbar.Brand href="/" className="Header-Logo">
-                                Spencer Marx
-                            </Navbar.Brand>
+                        <Link to="/" className="Header-Logo">
+                            Spencer Marx
                         </Link>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="ml-auto">
-                                <Nav.Link><Link to="/about" className="Header-Links">About</Link></Nav.Link>
-                                <Nav.Link><Link to="/contact" className="Header-Links">Contact</Link></Nav.Link>
+                                <Link to="/about" className="Header-Links">About</Link>
+                                <Link to="/contact" className="Header-Links">Contact</Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
