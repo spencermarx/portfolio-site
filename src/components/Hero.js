5b34c4edd073d179f0a90e283da3703acb9e2760
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-08 19:47:44 -0400

    Add TypedTitles component for dynamic Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import TypedTitles from './TypedTitles';
 import './hero.scss';
 
 class Hero extends Component {
+
     render() {
         return (
             <React.Fragment>
                 <section id="Hero" className="Hero">
                     <div className="Hero-Text">
-                        <h1 className="Hero-Text-Header" >Hi, I'm Spencer.<br />
-                        I'm a <strong>Full Stack Developer</strong><br/>
+                        <h1 className="Hero-Text-Header" >Hi, I'm Spencer, I'm a<br />
+                        <strong><TypedTitles/></strong><br/>
                          living in Boston.</h1>
                         <p className="Hero-Text-SubHeader">Background in UX Design and Geographic Information Science. Currently considering new work opportunities building impactful digital experiences.</p>
                     </div>
