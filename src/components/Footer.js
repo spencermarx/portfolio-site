bba948208454639c81b53a8e60d9f73e80c85aae
Spencer Marx <spencer.s.marx@gmail.com>
2021-02-02 15:39:26 -0500

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'gatsby';
 import SocialIcon from './SocialIcon';
 import './footer.scss';
 
-
-class Footer extends Component {
-
-    render() {
-        return (
-            <React.Fragment>
-                <footer className="Footer">
-                    <Container>
-                        <Row>
-                            <Col xs={12} md={6} className="Footer-Logo Logo">
-                                <Link to="/">Spencer Marx</Link>
-                            </Col>
-                            <Col className="col-12 col-md-6">
-                                <div className="Footer-SocialIcons d-flex justify-content-center justify-content-md-end">
-                                    <SocialIcon icon='email' color='black'/>
-                                    <SocialIcon icon="github-default" color='black'/>
-                                    <SocialIcon icon="linkedIn" color='black'/>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row className="Footer-Signature row mt-5">
-                            <Col className="text-center">
-                                <p className="my-0">© 2021 All Rights Reserved | Spencer Marx</p>
-                            </Col>
-                        </Row>
-                    </Container>
-                </footer>
-            </React.Fragment>
-        )
-    }
-}
-
-export default Footer;
+export default () => {
+  return (
+    <footer className="Footer">
+      <Container>
+        <Row>
+          <Col xs={12} md={6} className="Footer-Logo">
+            <Link className="Logo" to="/">
+              Spencer Marx
+            </Link>
+          </Col>
+          <Col className="col-12 col-md-6">
+            <div className="Footer-SocialIcons d-flex justify-content-center justify-content-md-end">
+              <SocialIcon icon="email" color="black" />
+              <SocialIcon icon="github-default" color="black" />
+              <SocialIcon icon="linkedIn" color="black" />
+            </div>
+          </Col>
+        </Row>
+        <Row className="Footer-Signature row mt-5">
+          <Col className="text-center">
+            <p className="my-0">© 2018 All Rights Reserved | Spencer Marx</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
