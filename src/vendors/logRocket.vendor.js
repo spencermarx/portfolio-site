393fd3787ee157d806f6ee00c449a322acf35154
Spencer Marx <spencer.s.marx@gmail.com>
2021-02-02 21:20:49 -0500

    Add log for logrocket

--- src/vendors/logRocket.vendor.js
+++ src/vendors/logRocket.vendor.js
@@ -1,6 +1,14 @@
 import LogRocket from 'logrocket';
 
 export default () => {
+  console.log(
+    JSON.stringify({
+      env: process.env,
+      PRODUCTION: process.env.PRODUCTION,
+      LOGROCKET_ENABLED: process.env.LOGROCKET_ENABLED,
+      LOGROCKET_KEY: process.env.LOGROCKET_KEY,
+    }),
+  );
   if (
     process.env &&
     process.env.PRODUCTION === 'true' &&
